5ce762bb5ae58b0ed56b3202845edb34a671c1bd
johnconstante-blip <john.constante@olivarezcollegetagaytay.edu.ph>
2025-11-14 06:25:05 +0000

    final commit

---
+++ js-activity-wk13/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const button = document.getElementById("btn");
+
+    button.addEventListener("click", function(){
+        const name = document.getElementById("name").value;
+        document.getElementById("result").textContent = "Hello, " + name + "!";
+    });
+});
